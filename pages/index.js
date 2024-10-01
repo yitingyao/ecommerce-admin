288eea311dd7a34eb21dd5951bb3a9eb84b7bf23
Yiting <yiting.tina.yao@outlook.com>
2024-09-30 21:50:36 -0400

    Added Google authentication

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 
 
 
 export default function Home() {
-  return (
-      <div className="bg-blue-900 w-screen h-screen itmes-center">
-        <div className="text-center">
-          <button className="bg-white p-2"> Login with Google</button>
-          </div>
+    const { data: session } = useSession()
+if (!session) {
+    return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+            <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg"> Login with Google</button>
+        </div>S
 
-      </div>
+    </div>
+);
+}
+    return (
+     <div> logged in {session.user.email}</div>
 
-  )
+  );
 }
