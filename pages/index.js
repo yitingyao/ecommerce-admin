74eef535ccc8c0bc8be8def436c959365276fb55
Yiting <yiting.tina.yao@outlook.com>
2024-10-05 08:44:12 -0400

    Fix authentication

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ if (!session) {
     <div className="bg-blue-900 w-screen h-screen flex items-center">
         <div className="text-center w-full">
             <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg"> Login with Google</button>
-        </div>S
+        </div>
 
     </div>
 );
